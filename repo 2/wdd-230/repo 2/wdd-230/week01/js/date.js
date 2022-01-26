ad7fa39b4105dbfc1d07caaebba5a2be811791b2
Juveju77 <97706637+Juveju77@users.noreply.github.com>
2022-01-26 12:58:29 -0500

    Delete repo 2/wdd-230 directory

--- repo 2/wdd-230/repo 2/wdd-230/week01/js/date.js
+++
@@ -1,9 +0,0 @@
-const d = new Date();
-let year = d.getFullYear();
-
-const copyright = document.querySelector('#copyright');
-copyright.textContent += `&copy; ${year} | Juvenato Mendes Neto | Boston`;
-
-
-const lastUpdated = document.querySelector('#lastMod');
-lastUpdated.textContent += `<strong>Last updated: ${document.lastModified}</strong>`;
