660529b500426fbe1c529ba6f35b771db4e4fa8d
Juveju77 <97706637+Juveju77@users.noreply.github.com>
2022-03-05 23:20:00 -0500

    nine

---
+++ repo 2/wdd-230/week09/directory.js
@@ -0,0 +1,55 @@
+// const requestURL = 'https://juveju77.github.io/wdd-230/repo%202/wdd-230/week09/data.json';
+const listButton = document.querySelector("#list-btn");
+const cardButton = document.querySelector("#card-btn");
+
+
+fetch(requestURL)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (jsonObject) {
+        const businesses = jsonObject['businesses'];
+        // console.table(jsonObject);
+        businesses.forEach(displayBusinesses);
+    });
+
+function displayBusinesses(business) {
+  let card = document.createElement('section');
+
+  //img
+  let image = document.createElement('img');
+  image.src = business.imgfile;
+  image.setAttribute('alt', business.name);
+  card.appendChild(image);
+
+  //h2 name
+  let h2 = document.createElement('h2');
+  h2.textContent = business.name;
+  card.appendChild(h2);
+
+  //p address
+  let p1 = document.createElement('p');
+  p1.textContent = business.address;
+  card.appendChild(p1);
+
+  //p phone
+  let p2 = document.createElement('p');
+  p2.textContent = business.phone;
+  card.appendChild(p2);
+
+  //p website
+  let p3 = document.createElement('p');
+  p3.textContent = business.website;
+  card.appendChild(p3);
+
+  cards.appendChild(card);
+
+}
+
+listButton.addEventListener("click", ()=> {
+    cards.classList.replace("card-view", "list-view")
+});
+
+cardButton.addEventListener("click", ()=> {
+    cards.classList.replace("list-view", "card-view")
+});
