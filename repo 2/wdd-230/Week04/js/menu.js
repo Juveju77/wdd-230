e1832cd3488e509f6706068b0e14c78714474146
Juveju77 <97706637+Juveju77@users.noreply.github.com>
2022-03-12 12:39:24 -0500

    Add files via upload

--- repo 2/wdd-230/Week04/js/menu.js
+++ repo 2/wdd-230/Week04/js/menu.js
@@ -1,36 +1,20 @@
 const menubutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.navigation');
 const exitbutton = document.querySelector('#xbutton');
-const hamburger = document.querySelector('#hambuger');
+const ham = document.querySelector('#ham');
 
 menubutton.addEventListener('click', () => {
     mainnav.classList.toggle('responsive');
     exitbutton.classList.toggle('hidden');
-    hamburger.classList.toggle('hidden');
+    ham.classList.toggle('hidden');
 }, false);
 
+// To solve the mid resizing issue with responsive class on
 window.onresize = () => {
     if (window.innerWidth > 760) {
         mainnav.classList.remove('responsive');
         exitbutton.classList.add("hidden");
-        hamburger.classList.remove("hidden");
+        ham.classList.remove("hidden");
     }
 };
 
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-
-
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
-
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-    mainMenu.style.top = '-100%';
-}
---
+++ repo 2/wdd-230/Week04/js/weather.js
@@ -0,0 +1,15 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Portland,OR,US&units=imperial&APPID=58efdcd1324b5f8f0322df4b0040eb55"
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  });
