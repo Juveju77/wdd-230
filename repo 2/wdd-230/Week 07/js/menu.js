d493f5d4c2dc7efa91f73ae8b47f278ee52c433f
Juveju77 <97706637+Juveju77@users.noreply.github.com>
2022-03-16 08:13:22 -0400

    Add files via upload

--- repo 2/wdd-230/Week 07/js/menu.js
+++ repo 2/wdd-230/Week 07/js/menu.js
@@ -1,19 +1,20 @@
 const menubutton = document.querySelector('.menu');
 const mainnav = document.querySelector('.navigation');
 const exitbutton = document.querySelector('#xbutton');
-const hamburger = document.querySelector('#hambuger');
+const ham = document.querySelector('#hamburger');
 
 menubutton.addEventListener('click', () => {
     mainnav.classList.toggle('responsive');
     exitbutton.classList.toggle('hidden');
-    hamburger.classList.toggle('hidden');
+    ham.classList.toggle('hidden');
 }, false);
 
+// To solve the mid resizing issue with responsive class on
 window.onresize = () => {
     if (window.innerWidth > 760) {
         mainnav.classList.remove('responsive');
         exitbutton.classList.add("hidden");
-        hamburger.classList.remove("hidden");
+        ham.classList.remove("hidden");
     }
 };
 
